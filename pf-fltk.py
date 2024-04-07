a1fed2536be21c40e40be84ee4a4f1dac41ef5a9
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-04-07 10:42:21 -0700

    Update fullscreen mode in pf-fltk.py

--- pf-fltk.py
+++ pf-fltk.py
@@ -35,6 +35,8 @@ def main(pics):
     
     root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
     root.overrideredirect(True)
+    root.config(cursor='none')
+    
     def end_fullscreen(event=None):
         root.overrideredirect(False)
 
